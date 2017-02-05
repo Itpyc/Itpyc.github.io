235d2b52f6c384f9fed69e07a40181a8da4a734b
Itpyc <1058500429@qq.com>
2017-02-05 15:16:10 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -91,6 +91,11 @@ self.addEventListener('activate', function (event) {
 self.addEventListener('fetch', function (event) {
   console.log(event.request)
  
+ event.respondWith(
+    caches.match(event.request.url).then(function (response) {
+      return response || fetch(event.request);
+    })
+  )
 });
 
 self.addEventListener('push', function (event) {
